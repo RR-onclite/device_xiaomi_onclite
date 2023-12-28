1b8aa38bff663304c2cce435ff57ed807f567957
BayerischeMotorenWerke <syaifulamri199@gmail.com>
2023-12-28 14:28:33 +0700

    onclite: RR
    Change-Id: I8040289284efc77461f4669b14aabbf278e201ae
    Signed-off-by: BayerischeMotorenWerke <syaifulamri199@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_onclite.mk
+    $(LOCAL_DIR)/rr_onclite.mk
+
+COMMON_LUNCH_CHOICES := \
+    rr_onclite-user \
+    rr_onclite-userdebug \
+    rr_onclite-eng
--- rr_onclite.mk
+++ rr_onclite.mk
@@ -12,11 +12,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Inherit from onclite device
 $(call inherit-product, device/xiaomi/onclite/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common ResurrectionRemix stuff.
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
+
+# Build Type
+RR_BUILDTYPE := Official
+TARGET_BOOT_ANIMATION_RES := 720
+TARGET_FACE_UNLOCK_SUPPORTED := true
+BUILD_RR_WALLPAPERS := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_onclite
+PRODUCT_NAME := rr_onclite
 PRODUCT_DEVICE := onclite
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 7
